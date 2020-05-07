5666b14360fd8d02cf85c34921de89dcf8a52946
Dieter Verschueren <dieterverschueren@gmail.com>
2020-05-07 13:14:07 +0200

    disable progress meter and add writeout

--- speedtest.sh
+++ speedtest.sh
@@ -9,7 +9,7 @@ $resuls
 _EOF_
 
 	data="speedtest,server=$serverid,sponsor=$sponsor,name=$name,ip=$ip ping=$ping,download=$download,upload=$upload,distance=$distance"
-	curl -XPOST "http://$DB_HOST/api/v2/write?bucket=$DB_NAME" --data-raw "$data"
+	curl -XPOST --no-progress-meter "http://$DB_HOST/api/v2/write?bucket=$DB_NAME" --data-raw "$data" -w "Result written to Influx, took %{time_total}s"
 	echo "Ping: $ping Download: $download - Upload: $upload  @  $timestamp"
 
 	sleep $TEST_INTERVAL
