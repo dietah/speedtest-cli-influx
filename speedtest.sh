1a121bc38389736847d5e8c4e1d522c15d771b21
Dieter Verschueren <dieterverschueren@gmail.com>
2020-05-07 13:23:17 +0200

    add line break

--- speedtest.sh
+++ speedtest.sh
@@ -10,6 +10,7 @@ _EOF_
 
 	data="speedtest,server=$serverid,sponsor=$sponsor,name=$name,ip=$ip ping=$ping,download=$download,upload=$upload,distance=$distance"
 	curl -XPOST --no-progress-meter "http://$DB_HOST/api/v2/write?bucket=$DB_NAME" --data-raw "$data" -w "Result written to Influx, took %{time_total}s"
+	echo
 	echo "Ping: $ping Download: $download - Upload: $upload  @  $timestamp"
 
 	sleep $TEST_INTERVAL
